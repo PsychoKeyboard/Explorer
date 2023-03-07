322bb1fce5236fde129550a7fdd146b54c794221
Ashish <ashparmar2000@gmail.com>
2023-03-06 18:55:19 -0600

    added cool comment

--- src/main/App.java
+++ src/main/App.java
@@ -3,6 +3,7 @@ import javax.swing.JFrame;
 
 public class App 
 {
+    //cool
     public static void main(String[] args) throws Exception 
     {
         //creates my window application where the game will play
